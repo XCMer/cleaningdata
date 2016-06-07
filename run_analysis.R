4bfc049607015d841bf3e45450ddb7f5c42d810c
Raahul Seshadri <raahul@musictypes.com>
2016-06-07 21:55:08 +0530

    Added initial reading of data

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+gatherData <- function(type = 'test') {
+  # Read activity tables. Since activities are numbered from 1
+  # in ascending order, the nth row gives us the label for the nth activity.
+  activity_labels <- read.table('activity_labels.txt', header = FALSE,
+                               col.names=c("number", "label"))
+  
+  # Read the features list. The same concept as activity above is applicable.
+  features <- read.table('features.txt', header = FALSE,
+                         col.names=c("number", "label"))
+  
+  # Initialize paths
+  base_path <- file.path(getwd(), type)
+  subject_path <- file.path(base_path, paste0("subject_", type, ".txt"))
+  X_path <- file.path(base_path, paste0("X_", type, ".txt"))
+  y_path <- file.path(base_path, paste0("y_", type, ".txt"))
+  
+  # Load data
+  subjects <- read.table(subject_path, header = FALSE, col.names=c("subjectnumber"))
+  
+  # We remove any column that does not have "mean" or "std" in its
+  # name, since we're only extacting mean and std deviation columns.
+  X_values <- read.table(X_path, header = FALSE, col.names=features[,'label'])
+  
+  required_X_columns <- grepl("(mean)|(std)", names(X_values))
+  X_values <- X_values[,required_X_columns]
+  
+  y_values <- read.table(y_path, header = FALSE, col.names=c("activitynumber"))
+
+  # Construct a dataset with all of the above things
+  clean_dataset = data.frame(
+    subjectnumber=subjects[,'subjectnumber'],
+    activitynumber=y_values[,'activitynumber']
+  )
+  
+  clean_dataset = cbind(clean_dataset, X_values)
+  
+  # Add activity name from number
+  clean_dataset$activityname <- sapply(clean_dataset$activitynumber,
+                                       function(x) {activity_labels[x,][['label']]})
+  
+  # Remove activitynumber since it's redundant
+  clean_dataset <- within(clean_dataset, rm(activitynumber))
+  
+  # Return the clean data set
+  clean_dataset
+}
